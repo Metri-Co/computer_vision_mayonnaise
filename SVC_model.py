698502786407e1a7a9efdb65331bd6172d82262a
Jorge Metri <87657676+Metri-Co@users.noreply.github.com>
2022-01-29 02:26:28 -0600

    Update SVC_model.py

--- SVC_model.py
+++ SVC_model.py
@@ -15,11 +15,6 @@ import mymetrics as mts
 from sklearn.model_selection import cross_validate
 
 
-#%%
-
-########################### Info gain preprocessing model ################################
-
-# import and split data
 df = pd.read_csv('results_mayonnaise_2_NORM.csv')
 X = np.asarray(df.loc[:,[ 'kurtosis_green',
                      'std_yellow',
@@ -52,54 +47,3 @@ for key in metrics:
     metrics[key].append(result["test_score"].mean())
     metrics[key].append(result["test_score"].std())
 
-
-#y_pred = my_svc.predict(X_test)
-# Metrics
-#svc_metrics = mts.create_metrics(y_test, y_pred)
-#svc_cmat = confusion_matrix(y_test, y_pred, labels=[0,1])
-#report = classification_report(y_test, y_pred, output_dict=True)
-
-#%%
-
-################################# Info Gain > 0.2 ################################
-
-X2 = np.asarray(df.loc[:,['Xsat',
-                        'Sdsat',
-                        'Sdb',
-                        'diff_gray',
-                        'maxb',
-                        'kurtosis_b',
-                        'Sdblue',
-                        'maxsat',
-                        'minsat',
-                        'Xb',
-                        'minblue',
-                        'kurtosis_hue',
-                        'entropy_diff',
-                        'skewness_hue',
-                        'homogeneity',
-                        'dissimilarity',
-                        'inv_diff',
-                        'kurtosis_saturation',
-                        'Xa',
-                        'meas_corr_2',
-                                    ]]) #metrics
-y2 = np.asarray(df.loc[:,"Grade"])
-
-my_svc_2 = SVC(C=9.5, kernel='rbf', gamma = 'auto', tol=0.001, cache_size=500, 
-             verbose=True, max_iter=- 1, decision_function_shape='ovr',
-             random_state=1)
-
-
-metrics2 = {"accuracy": [],
-           "precision": [],
-           "recall": [],
-           "f1": []
-    }
-for key in metrics2:
-    print("kfold cross validation for {}".format(key))
-    score = str(key)
-    result = cross_validate(my_svc_2, X2, y2, cv=10, scoring = score, n_jobs = 3)
-    metrics2[key].append(result["test_score"])
-    metrics2[key].append(result["test_score"].mean())
-    metrics2[key].append(result["test_score"].std())
